0a3a5a59ebd449b64bbb59c640c9a506d131f3e5
jade-nl <jade_nl@ziggo.nl>
2022-01-16 18:35:36 +0100

    Added script and updated README

--- Scripts/README.md
+++ Scripts/README.md
@@ -1,1 +1,15 @@
 # rt.extras -> Scripts
+
+### rt.bldr.sh
+
+Simple but flexible script to locally build RawTherapee's latest development version.
+
+It can Clone, Pull, Build and Install using either GCC or CLANG. There's also a section that makes it possible to include non-merged PRs.
+
+Make sure to change **gitBsDir** and **localTrgtDir** to reflect your situation. The rest of the settings are sane/safe to use out-of-the-box, but do have a look at the *configure* and the *set defaults* sections.
+
+### rtsm.sh
+
+A script that makes it possible to move the position of the created Local Adjustment spots.
+
+At the moment it is not possible to rearrange the spot order once they are created. There's an outstanding FR ([#6359](https://github.com/Beep6581/RawTherapee/issues/6359)) to get this implemented. This script fills that need for the time being.
---
+++ Scripts/rtsm.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+# -------------------------------------------------------------------------- #
+# Syntax  : rtsm.sh [/path/to/]name.pp3
+# -------------------------------------------------------------------------- #
+# Change the order of the Local Adjustment spots.
+# -------------------------------------------------------------------------- #
+# Mini Howto:
+#
+# 1) Save the profile from within RawTherapee.
+#    A full profile can be used or just the full Local Adjustment section
+#    using the *ctrl-click* facility.
+#    Do this manually. Do not count on the profile that RawTherapee writes
+#    on-the-fly to be up-to-date.
+# 2) Start the script with the saved profile as input file.
+# 3) Select which spot to move.
+# 4) Select new location of this spot.
+#    -repeat steps 3 and 4 if wanted/needed-
+# 5) press **x** when satisfied or **q** to abandon/quit.
+#
+# The first number (under *CL*) reflects the spot location, this is followed
+# by the spot name and (under *OL*) the original spot location (in case the
+# spot names are still the default *New Spot*).
+#
+# The script will not point out wrong choices, it will keep asking for
+# input until it is satisfied.
+#
+# If **x** was chosen then the script will create a new file that can be
+# imported into RawTherapee.
+# If **q* was chosen the script will stop running without doing anything.
+#
+# The saved profile will not be changed in any way, a new file will be
+#  created that holds the new layout.
+#
+# There's no need to quit RawTherapee while doing this, so the history will
+# not be lost and there's the possibility to undo by stepping back in the
+# history panel.
+# -------------------------------------------------------------------------- #
+#set -xv
+set -u
+umask 026
+LANG=POSIX; LC_ALL=POSIX; export LANG LC_ALL
+# -------------------------------------------------------------------------- #
+# input present
+[[ ! $# -eq 1 ]] && echo -e "\\n  Syntax -> rtsm.sh [/path/to/]file.pp3\\n" && exit 255
+# enough spots present
+spotCount=$(grep -Ecs "Name_[0-9]" "$1")
+if [ "${spotCount}" -le "1" ]
+then
+  echo ""
+  echo "  Not enough Local Adjustment spots found to continue."
+  echo ""
+  echo "   - A minimum of 2 spots is required."
+  echo ""
+  exit 254
+fi
+
+# -------------------------------------------------------------------------- #
+# Variables
+# ------------------------------------------------------------------ #
+inFile="$1"
+outFile="${inFile%.*}.la.pp3"
+tmpDir=$(mktemp -d -t rtsm-XXXXXXXXXX)
+declare -A manipArray
+declare -A spotsArray
+lglInSrc='^[1-9xq][0-9]*$'
+lglInTrgt='^[1-9][0-9]*$'
+cntr=""
+finalize="f"
+sptNUMB=1
+tmpOne=""
+tmpTwo=""
+lngDev="# -------------------------------------------------------------------------- #"
+
+# -------------------------------------------------------------------------- #
+# Functions
+# ------------------------------------------------------------------ #
+function _prtHeader ()
+{
+clear
+echo "${lngDev}"
+echo "#                   RawTherapee Local Adjustment spot mover                  #"
+echo "${lngDev}"
+}
+
+function _prtFooter ()
+{
+echo "# ${outFile} has been created."
+echo "# "
+echo "${lngDev}"
+}
+
+function _doMoves ()
+{
+while true
+do
+  _prtHeader
+  # -------------------------------------------------------- #
+  # print spots
+  cntr=1
+  echo "# CL : Spot Name                                                         OL  #"
+  echo "${lngDev}"
+  until [ $cntr -gt "$spotCount" ]
+  do
+    printf "# %2s : %3s\\n" "$cntr" "${spotsArray[${manipArray[$cntr]}]}"
+    (( cntr++ ))
+  done
+  echo "#"
+  
+  # -------------------------------------------------------- #
+  # get spot to move
+  while read -rp "# Select a spot [1-${spotCount}xq] : " srcSpot
+  do
+    if [ "${srcSpot}" = "x" ]
+    then
+      finalize="t"
+      echo -en "\\033[1A\\033[2K"
+      break
+    elif [ "${srcSpot}" = "q" ]
+    then
+      echo -en "\\033[1A\\033[2K"
+      echo "# -- quiting now."
+      echo "# -- changes are discarded, no output generated."
+      echo "# "
+      echo "${lngDev}"
+      rm -rf "${tmpDir}"
+      exit
+    fi
+    if ! [[ "${srcSpot}" =~  ${lglInSrc} ]]    || \
+         [[ "${srcSpot}" -lt 1 ]]              || \
+         [[ "${srcSpot}" -gt "${spotCount}" ]] 
+    then
+      echo -en "\\033[1A\\033[2K"
+    else
+      break
+    fi
+  done
+
+  # -------------------------------------------------------- #
+  # are we done moving
+  [ "${finalize}" = "t" ] && break
+  
+  # -------------------------------------------------------- #
+  # get location to move to
+  while read -rp "# Move to spot [0-${spotCount}] : " trgtSpot
+  do
+    if ! [[ "${trgtSpot}" =~  ${lglInTrgt} ]] || \
+         [[ "${trgtSpot}" -lt 0 ]]            || \
+         [[ "${trgtSpot}" -eq ${srcSpot} ]]
+    then
+      echo -en "\\033[1A\\033[2K"
+    else
+      break
+    fi
+  done
+
+  # -------------------------------------------------------- #
+  # update manipArray to new situation
+  if [[ "${srcSpot}" -lt "${trgtSpot}" ]]
+  then
+    # old spot < new spot
+    tmpOne="${manipArray[$srcSpot]}"
+    for (( tllr = srcSpot; tllr < trgtSpot; tllr++ ))
+    do
+      tmpTwo=$((tllr+1))
+      manipArray[$tllr]=${manipArray[$tmpTwo]}
+    done
+    manipArray[$trgtSpot]="$tmpOne"
+  else
+    # old spot > new spot
+    tmpOne="${manipArray[$srcSpot]}"
+    for (( tllr = srcSpot; tllr > trgtSpot; tllr-- ))
+    do
+      tmpTwo=$((tllr-1))
+      manipArray[$tllr]=${manipArray[$tmpTwo]}
+    done
+    manipArray[$trgtSpot]="$tmpOne"
+  fi
+done
+}
+
+function _reConstruct ()
+{
+  # -------------------------------------------------------- #
+  # recreate Locallab header
+  # set correct Selspot
+  selSpot=$( awk -F"=" '/Selspot/{ print $2}' "${tmpDir}/spot_0" )
+  (( csSpot = selSpot + 1 ))
+  for (( sp = 1; sp <= ${#manipArray[*]} ; sp++ ))
+  do
+    if [ "${manipArray[$sp]}" = ${csSpot} ]
+    then 
+      (( nsp = sp - 1 ))
+      sed 's/Selspot='"${selSpot}"'/Selspot='${nsp}'/' \
+        "${tmpDir}/spot_0" > "${outFile}" 
+    fi
+  done
+  # -------------------------------------------------------- #
+  # create Locallab spots
+  for (( sp = 1; sp <= ${#manipArray[*]} ; sp++ ))
+  do
+    (( nsp = sp - 1 ))
+    sed 's/_[0-9][0-9]*=/_'"${nsp}"'=/' \
+      "${tmpDir}/spot_${manipArray[$sp]}" >> "${outFile}"
+  done
+}
+
+# -------------------------------------------------------------------------- #
+# Main
+# ------------------------------------------------------------------ #
+# split LA section into singular spots
+awk -v tmpDir="${tmpDir}" '
+BEGIN { FS = "=" ; sptCntr = 0 }
+/\[Locallab\]/,/^$/ { 
+  if ( $1 ~ "Name_" ) { sptCntr++ }
+  { print >> tmpDir"/spot_"sptCntr }
+}
+' "${inFile}"
+
+# -------------------------------------------------------- #
+# fill arrays with initial data
+while read -r SPOT
+do
+  sptNAME="${SPOT#*=}"
+  formOut=$(printf "%-65s %02d" "$sptNAME" "$sptNUMB")
+  spotsArray[${sptNUMB}]="${formOut}"
+  manipArray[${sptNUMB}]="${sptNUMB}"
+  (( sptNUMB++ ))
+done < <( sed -n '/Name_/p' "${inFile}" )
+
+_doMoves
+
+_reConstruct
+
+_prtFooter
+
+# -------------------------------------------------------------------------- #
+# Cleanup
+rm -rf "${tmpDir}"
+
+exit 0
+
+# -------------------------------------------------------------------------- #
+# End
